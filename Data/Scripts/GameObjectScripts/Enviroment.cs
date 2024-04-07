24d4116c427f9d4ef1f7c575bd1fff20e008a017
Antonio <akezele35@gmail.com>
2024-04-07 14:37:59 +0200

    init

---
+++ Data/Scripts/GameObjectScripts/Enviroment.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviroment : InGameObject
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+
+
+
+}
